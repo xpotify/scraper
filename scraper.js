baa547c71b862af79d75dade9f582a1d1415d1de
cr <72098121+CyberRodeo@users.noreply.github.com>
2025-03-03 00:38:44 +0530

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,3 +1,25 @@
 const axios = require('axios'); 
-axios.get('https://www.scrapingcourse.com/ecommerce/') 
-	.then(({ data }) => console.log(data));
+const cheerio = require('cheerio'); 
+ 
+// axios.get('https://www.scrapingcourse.com/ecommerce/') 
+// 	.then(({ data }) => { 
+// 		const $ = cheerio.load(data); 
+ 
+// 		const productNames = $('.XUwMufC5NCgIyRMyGXLD') 
+// 			.map((_, product) => { 
+// 				const $product =  $product.find('.wozXSN04ZBOkhrsuY5i2 XUwMufC5NCgIyRMyGXLD'); 
+// 				return $product
+// 			}) 
+// 			.toArray(); 
+// 		console.log(productNames) 
+// 	});
+
+
+axios.get('https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ') 
+	.then(({ data }) => { 
+		const $ = cheerio.load(data);
+        
+        const $class = $('.XUwMufC5NCgIyRMyGXLD');
+
+        console.log($class);
+});
