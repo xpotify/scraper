0f652987d9504cc58ebd851f5b4d4c5ad602e5d1
cr <72098121+CyberRodeo@users.noreply.github.com>
2025-03-11 22:52:33 +0530

    Rewrote the function

--- scraper.js
+++ scraper.js
@@ -1,25 +1,24 @@
-const axios = require('axios'); 
-const cheerio = require('cheerio'); 
- 
-// axios.get('https://www.scrapingcourse.com/ecommerce/') 
-// 	.then(({ data }) => { 
-// 		const $ = cheerio.load(data); 
- 
-// 		const productNames = $('.XUwMufC5NCgIyRMyGXLD') 
-// 			.map((_, product) => { 
-// 				const $product =  $product.find('.wozXSN04ZBOkhrsuY5i2 XUwMufC5NCgIyRMyGXLD'); 
-// 				return $product
-// 			}) 
-// 			.toArray(); 
-// 		console.log(productNames) 
-// 	});
+const puppeteer = require('puppeteer');
 
+const fetchArtistBackground = async (artistLink) => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-axios.get('https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ') 
-	.then(({ data }) => { 
-		const $ = cheerio.load(data);
-        
-        const $class = $('.XUwMufC5NCgIyRMyGXLD');
+    await page.goto(artistLink, {
+        waitUntil: "networkidle2"
+    });
 
-        console.log($class);
-});
+    const element = await page.$('[data-testid="background-image"');
+
+    if(element){
+        const backgroundImageLink = await page.evaluate(el => el.style.backgroundImage.slice(5, -2), element);
+        console.log(backgroundImageLink);
+        return backgroundImageLink;
+    } else {
+        console.log("Background Image link was not found.")
+    }
+
+    await browser.close();
+};
+
+fetchArtistBackground("https://open.spotify.com/artist/7b6Ui7JVaBDEfZB9k6nHL0");
