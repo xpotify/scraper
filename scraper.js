ed1120e21b0acefa46d5513fbfb80092a7743ace
Shuiii <72098121+CyberRodeo@users.noreply.github.com>
2025-03-26 14:16:41 +0530

    Update scraper.js
    Co-Authored-By: asuuuu <159941275+hikenoesu@users.noreply.github.com>

--- scraper.js
+++ scraper.js
@@ -8,9 +8,11 @@ const fetchArtistBackground = async (artistLink) => {
         waitUntil: "networkidle2"
     });
 
-    const element = await page.$('[data-testid="background-image"');
+    const element = await page.$('div[data-testid="background-image"]');
     const leti = await page.$('mMx2LUixlnN_Fu45JpFB Ii9XdJaXIuKbmR1zC4Rt Yn2Ei5QZn19gria6LjZj');
     let letii = [];
+
+    console.log(element);
 
     if(leti.length > 1){
         for(i=0; i < leti.length; i++){
@@ -33,4 +35,4 @@ const fetchArtistBackground = async (artistLink) => {
     await browser.close();
 };
 
-fetchArtistBackground("https://open.spotify.com/artist/1XgFuvRd7r5g0h844A5ZUQ");
+fetchArtistBackground("https://open.spotify.com/artist/0LcJLqbBmaGUft1e9Mm8HV");
